123f0847fa0080ed25f9d4101e576553c2f48976
Victoria Jordan <vjordan.cs@gmail.com>
2022-11-15 12:26:49 -0600

    add csv files for node creation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import csv
 
 
 class Vote:
@@ -8,9 +9,9 @@ class Vote:
     votes = []
     INITIAL_TRUST = 1
 
-    def __init__(self, voterName, voterDLnum, voterTimeCast, voterParty, voterIPaddress, voterMACaddress, voterVerifNum, INITIAL_TRUST):
+    def __init__(self, voterNum, voterName,  voterTimeCast, voterParty, voterIPaddress, voterMACaddress, voterVerifNum, INITIAL_TRUST):
+        self.DLnum = voterNum
         self.name = voterName
-        self.DLnum = voterDLnum
         self.timeCast = voterTimeCast
         self.party = voterParty
         self.IPaddress = voterIPaddress
@@ -18,9 +19,8 @@ class Vote:
         self.verifNum = voterVerifNum
         self.trust = INITIAL_TRUST
 
-
 # node types
-#validVote = Vote()
+# validVote = Vote(var_name[0])
 #corruptedVote = Vote()
 #corruptedVoteFromVote = Vote()
 
@@ -35,14 +35,36 @@ def printReport():
     pass
 
 
-def init():
-    pass
+def initDirectedGraph():
+    G = nx.DiGraph()
+    # G.add_edges_from([('A', 'B'), ('A', 'C'), ('C', 'B'), ('D', 'A')])
+    # pos = nx.spring_layout(G)
+    # nx.draw_networkx_nodes(G, pos, node_size=500)
+    # nx.draw_networkx_edges(G, pos, edgelist=G.edges(), edge_color='black')
+    # nx.draw_networkx_labels(G, pos)
+    # plt.show()
+    voterPairs = []
+    with open('Voter-Pairs.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                line_count += 1
+                voter1 = row[0]
+                voter2 = row[1]
+                print(voter1, voter2)
+                voterPairs.append((voter1, voter2))
+
+    G.add_edges_from(voterPairs)
+    # print(G.number_of_nodes())
+    pos = nx.spring_layout(G)
+    # nx.draw_networkx_nodes(G, pos, node_size=100)
+    nx.draw_networkx_edges(G, pos, edgelist=G.edges(), edge_color='black')
+    nx.draw_networkx_labels(G, pos, font_size=7)
+    nx.draw(G)
+    plt.show()
 
 
-G = nx.DiGraph()
-G.add_edges_from([('A', 'B'), ('A', 'C'), ('C', 'B')])
-pos = nx.spring_layout(G)
-nx.draw_networkx_nodes(G, pos, node_size=500)
-nx.draw_networkx_edges(G, pos, edgelist=G.edges(), edge_color='black')
-nx.draw_networkx_labels(G, pos)
-plt.show()
+initDirectedGraph()
